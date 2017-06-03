bdafdc202adf9e8614943af738b75d1e3d5d4891
Jason Lau <i@dotkrnl.com>
2017-06-03 11:34:31 +0800

    Unfinished blocks

--- ramfs.cpp
+++ ramfs.cpp
@@ -5,6 +5,7 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <cstdint>
 #include <map>
 #include <string>
 #include <cerrno>
@@ -19,6 +20,9 @@
 #define MSIZE          2
 #define ROOT           0
 #define PID_MAX        32768
+#define BLOCK_SIZE     (128 * 1024)
+#define BLOCK_TABLE_GROW (1024 * 1024)
+#define BLOCK_POOL_GROW  (1024)
 
 int rank, nprocs, provided;
 
@@ -31,11 +35,32 @@ struct fsdata
     unsigned long avail_no_of_files;
 };
 
+union block
+{
+    uint8_t data[BLOCK_SIZE];
+    union block *next;
+};
+
+union block *block_continuous_pool;
+size_t block_continuous_pool_size;
+
+union block *block_free_chain;
+
+struct block_info
+{
+    union block *addr;
+    int rank;
+};
+
 struct metadata
 {
     unsigned long inode;
     unsigned long size;
-    char *data;
+
+    char *data;  // just for dir
+    block_info *blocks_table;
+    size_t blocks_table_size;
+
     mode_t mode;
     short inuse;
     time_t accesstime;
@@ -76,6 +101,7 @@ struct request_message
     int source;
     int size;
     char *base;
+    bool end;
 };
 
 struct meta_message
@@ -97,6 +123,58 @@ typedef std::pair<std::string, std::string> path_pair;
 typedef std::pair<path_pair, unsigned long> file_pair;
 typedef std::map<path_pair, unsigned long>::iterator files_iter;
 std::map<path_pair, unsigned long> files;
+
+inline union block *alloc_block()
+{
+    union block *ret;
+    if (block_free_chain != NULL) {
+        ret = block_free_chain;
+        block_free_chain = block_free_chain->next;
+    } else {
+        if (block_continuous_pool_size <= 0) {
+            block_continuous_pool = (union block *)malloc(BLOCK_POOL_GROW *
+	            sizeof(union block *));
+            block_continuous_pool_size = BLOCK_POOL_GROW;
+        }
+        ret = block_continuous_pool;
+        block_continuous_pool++;
+        block_continuous_pool_size--;
+    }
+    memset(ret, 0, sizeof(block_free_chain));
+    return ret;
+}
+
+inline void free_block(union block *b)
+{
+    if (b == NULL) return;
+    b->next = block_free_chain;
+    block_free_chain = b;
+}
+
+inline block_info *resize_block_table(block_info *table,
+        size_t orig_size, size_t size)
+{
+    size_t orig_grows = orig_size / BLOCK_TABLE_GROW + 1;
+    size_t required_grows = size / BLOCK_TABLE_GROW + 1;
+
+    if (required_grows == orig_grows) return table;
+
+    block_info *ret;
+    if (table == NULL) {
+        orig_grows = 0;
+        ret = (block_info *)malloc(required_grows * BLOCK_TABLE_GROW *
+                sizeof(block_info));
+    } else {
+        ret = (block_info *)realloc(table, required_grows *
+                BLOCK_TABLE_GROW * sizeof(block_info));
+    }
+
+    if (required_grows > orig_grows)
+        memset(ret + orig_grows, 0, (required_grows - orig_grows) *
+                BLOCK_TABLE_GROW * sizeof(block_info));
+
+    return ret;
+}
 
 void get_dirname_filename ( const char *path, char *dir_name, char *base_name )
 {
@@ -135,7 +213,8 @@ int fill_file_data(char *dirname, char *fname, mode_t mode, uid_t uid, gid_t gid
 
     file[i].inode = i;
     file[i].size = 0;
-    file[i].data = NULL;
+    file[i].blocks_table = NULL;
+    file[i].blocks_table_size = 0;
     file[i].inuse = 1;
     file[i].mode = S_IFREG | mode;
     file[i].accesstime = time(NULL);
@@ -465,17 +544,33 @@ static int server_truncate(const char *path, off_t offset )
 
     if ( offset == 0 )
     {
-        free(file [iter->second].data);
-        file [iter->second].data = NULL;
+        for (int idx = 0; idx < file[iter->second].blocks_table_size; idx++) {
+            if (file[iter->second].blocks_table[idx].addr)
+                free_block(file[iter->second].blocks_table[idx].addr);
+        }
+        free(file[iter->second].blocks_table);
+        file[iter->second].blocks_table = NULL;
+        file[iter->second].blocks_table_size = 0;
         file [iter->second].size = 0;
         fs_stat.free_bytes = fs_stat.free_bytes + old_size ;
         fs_stat.used_bytes = fs_stat.used_bytes - old_size ;
     }
     else
     {
-
-        file [iter->second].data = (char *) realloc( file[iter->second].data, offset + 1);
-        file [iter->second].size = offset + 1;
+        int block_id = (offset + 1) / BLOCK_SIZE;
+        int new_size = block_id + 1;
+        for (int idx = new_size;
+                idx < file[iter->second].blocks_table_size; idx++) {
+            if (file[iter->second].blocks_table[idx].addr)
+                free_block(file[iter->second].blocks_table[idx].addr);
+            file[iter->second].blocks_table[idx].addr = NULL;
+        }
+        file[iter->second].blocks_table = resize_block_table(
+                file[iter->second].blocks_table,
+                file[iter->second].blocks_table_size,
+                new_size);
+        file[iter->second].blocks_table_size = new_size;
+        file[iter->second].size = offset + 1;
         fs_stat.free_bytes = fs_stat.free_bytes + old_size - offset + 1;
         fs_stat.used_bytes = fs_stat.used_bytes - old_size + offset + 1;
     }
@@ -601,31 +696,69 @@ static int imfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off
     return 0;
 }
 
-static int server_read(const char *path, size_t size, off_t offset, request_message &request)
+static void server_read(command &comm)
 {
-    files_iter iter = find_file(path);
+#define SEND_MPI MPI_Send(&request, sizeof(request), MPI_CHAR, comm.source, 1, MPI_COMM_WORLD);
+    request_message request;
 
-    if ( iter == files.end() )
-        return -ENOENT;
-
-    if (S_ISDIR(file[iter->second].mode))
-        return -EISDIR;
-
-    request.source = rank;
-
-    if ( file[iter->second].data != NULL  &&  ( (unsigned long)offset < file[iter->second].size ) )
-    {
-        if (offset + size > file[iter->second].size )
-            size = file[iter->second].size - offset;
-
-        request.base = file[iter->second].data + offset;
+    files_iter iter = find_file(comm.path);
+    if (iter == files.end()) {
+        request.end = true;
+        request.size = -ENOENT;
+        SEND_MPI; return;
     }
-    else
-        size = 0;
+
+    if (S_ISDIR(file[iter->second].mode)) {
+        request.end = true;
+        request.size = -EISDIR;
+        SEND_MPI; return;
+    }
 
     file[iter->second].accesstime = time(NULL);
 
-    return size;
+    block_info *table = file[iter->second].blocks_table;
+    if (comm.offset + comm.size > file[iter->second].size)
+        comm.size = file[iter->second].size - comm.offset;
+    if (comm.size <= 0) {
+        request.end = true;
+        request.size = 0;
+        SEND_MPI; return;
+    }
+
+    int offset = comm.offset, size = comm.size;
+    int endpos = offset + size;
+    int blkno = offset / BLOCK_SIZE;          // The NO. of rd begin block
+    int nblks = endpos / BLOCK_SIZE - blkno;  // The size of rd blocks
+
+    request.type = request.get;
+    request.end = false;
+    int blkoff = offset % BLOCK_SIZE;
+    if (blkoff != 0) {
+        int rs = (nblks != 0) ? (BLOCK_SIZE - blkoff) : (endpos - offset);
+        request.source = table[blkno].rank;
+        request.size = rs;
+        SEND_MPI;
+        blkno++, nblks--;
+    }
+    if (nblks + 1 == 0) goto out;
+
+    request.size = BLOCK_SIZE;
+    while (nblks != 0) {
+        request.source = table[blkno].rank;
+        SEND_MPI;
+        blkno++, nblks--;
+    }
+
+    request.size = endpos % BLOCK_SIZE;
+    if (request.size != 0) {
+        request.source = table[blkno].rank;
+        SEND_MPI;
+    }
+
+out:
+    request.end = true;
+    SEND_MPI;
+#undef SEND_MPI
 }
 
 static int imfs_read(const char *path, char *buf, size_t size, off_t offset, fuse_file_info *fi)
@@ -638,16 +771,20 @@ static int imfs_read(const char *path, char *buf, size_t size, off_t offset, fus
     MPI_Send(&comm, sizeof(comm), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 
     request_message request;
-    MPI_Recv(&request, sizeof(request), MPI_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    int total = 0;
 
-    if (request.size <= 0) return request.size;
+    for (; ; ) {
+        MPI_Recv(&request, sizeof(request), MPI_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (request.size < 0) return request.size;
+        if (request.end) break;
+        int target = request.source;
+        request.source = rank;
+        MPI_Send(&request, sizeof(request), MPI_CHAR, target, 2, MPI_COMM_WORLD);
+        MPI_Recv(buf, request.size, MPI_CHAR, target, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        total += request.size;
+    }
 
-    int target = request.source;
-    request.source = rank;
-    MPI_Send(&request, sizeof(request), MPI_CHAR, target, 2, MPI_COMM_WORLD);
-    MPI_Recv(buf, request.size, MPI_CHAR, target, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-    return request.size;
+    return total;
 }
 
 static int server_write(const char *path, size_t size, off_t offset, request_message &request)
@@ -662,25 +799,25 @@ static int server_write(const char *path, size_t size, off_t offset, request_mes
     if (S_ISDIR(file[iter->second].mode))
         return -EISDIR;
 
-    if ( file [iter->second].data == NULL )
+    //if ( file [iter->second].data == NULL )
     {
         if ( fs_stat.free_bytes < size )
             return -ENOSPC;
 
-        file [iter->second].data = (char *) malloc( offset + size);
+        //file [iter->second].data = (char *) malloc( offset + size);
 
-        if ( file [iter->second].data == NULL )
+        //if ( file [iter->second].data == NULL )
         {
             perror("malloc:");
             return -ENOMEM;
         }
 
-        memset(file [iter->second].data, 0, offset + size);
+        //memset(file [iter->second].data, 0, offset + size);
         file [iter->second].size = offset + size;
         fs_stat.free_bytes = fs_stat.free_bytes - (offset + size);
         fs_stat.used_bytes = fs_stat.used_bytes + offset + size;
     }
-    else
+    //else
     {
 
         old_size = file [iter->second].size;
@@ -690,7 +827,7 @@ static int server_write(const char *path, size_t size, off_t offset, request_mes
             if ( fs_stat.free_bytes < ( offset + size - old_size ) )
                 return -ENOSPC;
 
-            file [iter->second].data = (char *) realloc( file[iter->second].data, (offset + size) );
+            //file [iter->second].data = (char *) realloc( file[iter->second].data, (offset + size) );
             fs_stat.free_bytes = fs_stat.free_bytes + old_size - ( offset + size );
             fs_stat.used_bytes = fs_stat.used_bytes - old_size + ( offset + size );
             file [iter->second].size = offset + size;
@@ -698,7 +835,7 @@ static int server_write(const char *path, size_t size, off_t offset, request_mes
     }
 
     request.source = rank;
-    request.base = file[iter->second].data + offset;
+    //request.base = file[iter->second].data + offset;
     file[iter->second].accesstime = time(NULL);
     file[iter->second].modifiedtime = time(NULL);
     return size;
@@ -797,8 +934,8 @@ static int server_unlink(const char *path)
 
 
     file [iter->second].inuse = 0;
-    free(file [iter->second].data);
-    file [iter->second].data = NULL;
+    //free(file [iter->second].data);
+    //file [iter->second].data = NULL;
 
     fs_stat.free_bytes = fs_stat.free_bytes + file [iter->second].size + sizeof ( file_pair );
     fs_stat.used_bytes = fs_stat.used_bytes - file [iter->second].size - sizeof ( file_pair );
@@ -835,8 +972,8 @@ static int server_rmdir(const char *path)
         return -EBUSY;
 
     file [iter->second].inuse = 0;
-    free(file [iter->second].data);
-    file [iter->second].data = NULL;
+    //free(file [iter->second].data);
+    //file [iter->second].data = NULL;
 
     fs_stat.free_bytes = fs_stat.free_bytes + file [iter->second].size + sizeof ( file_pair );
     fs_stat.used_bytes = fs_stat.used_bytes - file [iter->second].size - sizeof ( file_pair );
@@ -932,7 +1069,7 @@ static void server_destroy ()
 
     for (size_t i = 0; i < fs_stat.max_no_of_files; i++ )
     {
-        free(file [i].data);
+        //free(file [i].data);
     }
 
     free(file);
@@ -1063,11 +1200,7 @@ void server_loop()
             case command::read:
             {
                 //printf("\t%s, %ld, %ld\n", comm.path, comm.offset, comm.size);
-                request_message request;
-                int ret = server_read(comm.path, comm.size, comm.offset, request);
-                request.size = ret;
-                request.type = request.get;
-                MPI_Send(&request, sizeof(request), MPI_CHAR, comm.source, 1, MPI_COMM_WORLD);
+                server_read(comm);
                 break;
             }
 
