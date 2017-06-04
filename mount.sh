21cb2c8c80a3bd9b494eef64596580be17c8405d
David Feng <jiguanglizipao@163.com>
2017-06-04 20:22:28 +0800

    fix size bug

--- mount.sh
+++ mount.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./ramfs $1 $2 -f -o allow_other,default_permissions,large_read,big_writes -s
+./ramfs $1 $2 -f -o allow_other,default_permissions,large_read,big_writes,auto_cache,direct_io -s
